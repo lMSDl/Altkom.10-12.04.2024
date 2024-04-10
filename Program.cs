14de06afb2d44c63a79abf2bf723b988c904afc7
Paweł Łęczycki <pawel@msd.info.pl>
2024-04-10 12:31:42 +0200

    LSP

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Altkom._10_12._04._2024.SOLID.L;
+
+int a = 4;
+int b = 9;
+
+
+var rectangle = CreateRectangle(a, b);
+
+
+Console.WriteLine($"{a} * {b} = {rectangle.Area}");
+
+Rectangle CreateRectangle(int a, int b)
+{
+    return new Square() { A = a, B = b };
+}
---
+++ SOLID/LSP_1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._10_12._04._2024.SOLID.L
+{
+    public abstract class Vehicle
+    {
+        public string Name { get; set; }
+        public abstract void Move();
+    }
+
+    public class Airplain : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("I am flying!");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("I am driving!");
+        }
+    }
+
+
+    /*abstract class Vehicle
+    {
+        public string Name { get; set; }
+        public abstract void Fly();
+    }
+    class Airplane : Vehicle
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("I am flying!");
+        }
+    }
+    class Car : Vehicle
+    {
+        public override void Fly()
+        {
+            throw new NotImplementedException();
+        }
+    }*/
+}
