c0ee38555ff962586a2ab6d198e07b4de209da35
Paweł Łęczycki <pawel@msd.info.pl>
2024-04-10 10:22:05 +0200

    KISS

---
+++ DesignPrinciples/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace Altkom._10_12._04._2024.DesignPrinciples
+{
+    public class Customer
+    {
+        public Customer(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string CustomerName { get; set; }
+        public float Incomes { get; set; }
+        public float Outcomes { get; set; }
+        public float AllowedDebit { get; set; }
+
+        public bool IsDebit()
+        {
+            return Incomes - Outcomes < 0;
+        }
+    }
+}
---
+++ DesignPrinciples/PaymentService.cs
@@ -0,0 +1,44 @@
+﻿namespace Altkom._10_12._04._2024.DesignPrinciples
+{
+    public class PaymentService
+    {
+        private ICollection<Customer> Customers { get; } = new List<Customer> { new Customer(1), new Customer(2), new Customer(3), new Customer(4), new Customer(5) };
+
+        public bool DeleteCustomer(Customer customer)
+        {
+            return Customers.Remove(customer);
+        }
+
+        public Customer? FindByAllowedDebit(float allowedDebit)
+        {
+            return Customers.SingleOrDefault(x => x.AllowedDebit == allowedDebit);
+        }
+
+        public bool Charge(int id, float amount)
+        {
+            var customer = Customers.SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                return false;
+
+            if (customer.Incomes - customer.Outcomes + customer.AllowedDebit < amount)
+                return false;
+
+            customer.Outcomes += amount;
+            return true;
+        }
+
+        public void Fund(int id, float amount)
+        {
+            var customer = Customers.Where(x => x.Id == id).SingleOrDefault();
+            if (customer == null)
+                return;
+            customer.Incomes += amount;
+        }
+
+        public float? GetBalance(int id)
+        {
+            var customer = Customers.Where(x => x.Id == id).SingleOrDefault();
+            return customer?.Incomes - customer?.Outcomes;
+        }
+    }
+}
