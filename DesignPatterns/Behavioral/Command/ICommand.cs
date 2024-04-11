d00db5c41c38d530a6ce532c9afbd961b6d5e138
Paweł Łęczycki <pawel@msd.info.pl>
2024-04-11 15:21:03 +0200

    Command

---
+++ DesignPatterns/Behavioral/Command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._10_12._04._2024.DesignPatterns.Behavioral.Command
+{
+    internal interface ICommand
+    {
+        bool Execute();
+        void Reverse();
+    }
+}
