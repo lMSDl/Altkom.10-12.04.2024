29715230bec72e67fe54666a0ef22fcb7b4edc3d
Paweł Łęczycki <pawel@msd.info.pl>
2024-04-11 12:33:08 +0200

    Flyweight

---
+++ DesignPatterns/Structural/Flyweight/Car.cs
@@ -0,0 +1,32 @@
+﻿namespace Altkom._10_12._04._2024.DesignPatterns.Structural.Flyweight
+{
+    internal class Car
+    {
+        public required string Owner { get; set; }
+        public required string Number { get; set; }
+        public string Manufacturer
+        {
+            get => CarFlyweight.Manufacturer;
+            set => SetValue(x => x.Manufacturer = value);
+        }
+        public string Model
+        {
+            get => CarFlyweight.Model;
+            set => SetValue(x => x.Model = value);
+        }
+        public string Color
+        {
+            get => CarFlyweight.Color;
+            set => SetValue(x => x.Color = value);
+        }
+
+        public CarFlyweight CarFlyweight { get; set; } = new CarFlyweight();
+
+        private void SetValue(Action<CarFlyweight> action)
+        {
+            var clone = (CarFlyweight)CarFlyweight.Clone();
+            action(clone);
+            CarFlyweight = FlyweightFactory.Instance.GetFlyweight(clone);
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Flyweight/CarFlyweight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._10_12._04._2024.DesignPatterns.Structural.Flyweight
+{
+    internal class CarFlyweight : ICloneable
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
