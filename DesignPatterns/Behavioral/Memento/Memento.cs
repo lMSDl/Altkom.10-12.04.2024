d276a73b8c8d8b3fb35381cfc1ecd1563b49de8a
Paweł Łęczycki <pawel@msd.info.pl>
2024-04-12 10:48:43 +0200

    Memento

---
+++ DesignPatterns/Behavioral/Memento/Memento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._10_12._04._2024.DesignPatterns.Behavioral.Memento
+{
+    internal class Memento<T> where T : ICloneable
+    {
+        private T _state;
+        public DateTime DateTime { get; }
+
+        public Memento(T originator)
+        {
+            _state = (T)originator.Clone();
+            DateTime = DateTime.Now;
+        }
+
+        public T GetState()
+        {
+            return (T)_state.Clone();
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Memento/Person.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Altkom._10_12._04._2024.DesignPatterns.Behavioral.Memento
+{
+    internal partial class Person : ObservableObject, ICloneable, IRestorable<Person>
+    {
+        [ObservableProperty]
+        private string lastName;
+        [ObservableProperty]
+        private string firstName;
+
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public void Restore(Person state)
+        {
+            firstName = state.FirstName;
+            lastName = state.LastName;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
