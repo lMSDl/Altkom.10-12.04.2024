0d40c832b89b51bfb7fc2548ba2274a303173adc
Paweł Łęczycki <pawel@msd.info.pl>
2024-04-12 11:39:17 +0200

    Strategy

---
+++ DesignPatterns/Behavioral/Strategy/ICalcStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._10_12._04._2024.DesignPatterns.Behavioral.Strategy
+{
+    internal interface ICalcStrategy
+    {
+        float Calc(float a, float b);
+    }
+}
