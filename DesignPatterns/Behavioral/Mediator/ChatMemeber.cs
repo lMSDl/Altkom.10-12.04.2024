50d035bda77fe4deb6f67aa6417a4e613bb26aeb
Paweł Łęczycki <pawel@msd.info.pl>
2024-04-12 09:26:40 +0200

    Mediator

---
+++ DesignPatterns/Behavioral/Mediator/ChatMemeber.cs
@@ -0,0 +1,22 @@
+﻿namespace Altkom._10_12._04._2024.DesignPatterns.Behavioral.Mediator
+{
+    internal abstract class ChatMemeber
+    {
+        private IMediator mediator;
+        public string Nick { get; set; }
+        public IMediator Mediator
+        {
+            get => mediator;
+            set
+            {
+                if (mediator != null)
+                    mediator.Quit(this);
+                mediator = value;
+                if (mediator != null)
+                    mediator.Join(this);
+            }
+        }
+
+        public abstract void Receive(string from, string message, bool isPrivate);
+    }
+}
---
+++ DesignPatterns/Behavioral/Mediator/IMediator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._10_12._04._2024.DesignPatterns.Behavioral.Mediator
+{
+    internal interface IMediator
+    {
+        void Join(ChatMemeber member);
+        void Quit(ChatMemeber member);
+        void Send(string from, string message);
+        void Send(string from, string to, string message);
+    }
+}
